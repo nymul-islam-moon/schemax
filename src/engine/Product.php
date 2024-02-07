57ef29f70c4e1573d2897f98d7fbbf43fac2088e
Nymul Islam Moon <nymulislam.webappick@gmail.com>
2024-02-07 12:44:21 +0600

    product class method refactoring

--- src/engine/Product.php
+++ src/engine/Product.php
@@ -197,27 +197,27 @@ class Product
     protected function offers(): array {
 
         $offers = [
-            "@type" => "Offer",
-            "price" => $this->product->get_regular_price() ? $this->product->get_regular_price() : '',
-            "priceCurrency" => get_woocommerce_currency() ? get_woocommerce_currency() : '',
-            "priceSpecification" => $this->offers_priceSpecification(),
-            "priceValidUntil" => $this->offers_priceValidUntil(),
-            "priceValidFrom" => $this->offers_priceValidFrom(),
-            "availability" => $this->product->get_stock_status() ? $this->product->get_stock_status() : '',
-            "quantity" => $this->product->get_stock_quantity() != null ? $this->product->get_stock_quantity() : '',
-            "url" => $this->product->get_permalink() ? $this->product->get_permalink() : '',
-            "seller" => $this->offers_saller(),
-            "itemCondition" => $this->offers_itemCondition(),
-            "category" => !empty($this->offers_category()['name']) ? $this->offers_category()['name'] : '',
-            "mpn" => "working in progress",
-            "gtin8" => "working in progress",
-            "gtin13" => "working in progress",
-            "gtin14" => "working in progress",
-            "weight" => $this->product->has_weight() ? $this->offers_weight() : '',
-            "depth" => !empty($this->offers_depth()['value']) ? $this->offers_depth() : '',
-            "width" => !empty($this->offers_width()['value']) ? $this->offers_width() : '',
-            "height" => !empty($this->offers_height()['value']) ? $this->offers_height() : '',
-            "shippingDetails" => $this->offers_shippingDetails()
+            "@type"                 => "Offer",
+            "price"                 => $this->product->get_regular_price() ? $this->product->get_regular_price() : '',
+            "priceCurrency"         => get_woocommerce_currency() ? get_woocommerce_currency() : '',
+            "priceSpecification"    => $this->offers_priceSpecification(),
+            "priceValidUntil"       => $this->offers_priceValidUntil(),
+            "priceValidFrom"        => $this->offers_priceValidFrom(),
+            "availability"          => $this->product->get_stock_status() ? $this->product->get_stock_status() : '',
+            "quantity"              => $this->product->get_stock_quantity() != null ? $this->product->get_stock_quantity() : '',
+            "url"                   => $this->product->get_permalink() ? $this->product->get_permalink() : '',
+            "seller"                => $this->offers_saller(),
+            "itemCondition"         => $this->offers_itemCondition(),
+            "category"              => !empty($this->offers_category()['name']) ? $this->offers_category()['name'] : '',
+            "mpn"                   => "working in progress",
+            "gtin8"                 => "working in progress",
+            "gtin13"                => "working in progress",
+            "gtin14"                => "working in progress",
+            "weight"                => $this->product->has_weight() ? $this->offers_weight() : '',
+            "depth"                 => !empty($this->offers_depth()['value']) ? $this->offers_depth() : '',
+            "width"                 => !empty($this->offers_width()['value']) ? $this->offers_width() : '',
+            "height"                => !empty($this->offers_height()['value']) ? $this->offers_height() : '',
+            "shippingDetails"       => $this->offers_shippingDetails()
         ];
 
         return $offers;
