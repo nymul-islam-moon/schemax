d7933d90ccdd249a9a62365eeec7e720355b08ed
Nymul Islam Moon <towkir1997islam@gmail.com>
2024-02-14 14:20:00 +0600

    schemax updated for woocommerce not detected

--- src/Init.php
+++ src/Init.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace Schema;
+
+include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+
 use Schema\Engine\Product;
 use Schema\Engine\Website;
+
 
 class Init {
 
@@ -11,15 +15,18 @@ class Init {
 	}
 
 	public function run() {
-        if ( is_product() ) {
-            global $post;
-            $product_object = new Product( $post->ID );
-            $product_object->attach_schema();
-        } elseif ( ! is_admin() && ! defined( 'DOING_AJAX' ) ) {
+
+        if (is_plugin_active('woocommerce/woocommerce.php')) {
+            if ( is_product() ) {
+                global $post;
+                $product_object = new Product( $post->ID );
+                $product_object->attach_schema();
+            }
+        }
+
+        if ( ! is_admin() && ! defined( 'DOING_AJAX' ) ) {
             $website_object = new Website();
             $website_object->attach_schema();
-        } else {
-
         }
 
 	}
